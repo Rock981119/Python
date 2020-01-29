1418e02dfe41670d0ebcba5d5c41ba186a0139b2
Rock981119 <60067590+Rock981119@users.noreply.github.com>
2020-01-29 11:30:19 +0800

    Add files via upload

---
+++ p3ex4_error.py
@@ -0,0 +1,13 @@
+x = 42
+y = 0
+print()
+#try:
+print(x / 0)
+# except ZeroDivisionError as ex:
+#     print("错误，不允许被除数为零，请尝试别的数字")
+# else:
+#     print("something else went wrong")
+# finally:
+#     print("This is our cleanup ")
+
+print()
